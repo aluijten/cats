0082a08097fcc61c09fb8d33d697991238872e6a
aluijten <a.luijten@gmx.at>
2025-11-15 15:11:23 +0100

    update auto Beispiele

--- auto.py
+++ auto.py
@@ -1,6 +1,8 @@
 class Auto:
 
-    anzahl_reifen = 4                           # Klassenvariable
+    ANZAHL_REIFEN = 4                           # Klassenvariable
+    winterreifen_noetig = False                 # Klassenvariable
+    anzahl_autos = 0
 
     def __init__(self, marke, farbe, sitze, kilometerstand):
         #print("Init methode")
@@ -8,6 +10,7 @@ class Auto:
         self.farbe = farbe                      # Instanzvariable
         self.sitze = sitze                      # Instanzvariable
         self.kilometerstand = kilometerstand    # Instanzvariable
+        Auto.anzahl_autos += 1
     
     def __str__(self):
         return f"Der {self.marke} ist {self.farbe} und hat {self.sitze} Sitze. Der Kilometerstand ist {self.kilometerstand}"
@@ -28,9 +31,16 @@ auto1 = Auto("BMW", "rot", 5 , 30000)
 
 auto2 = Auto("VW", "blau", 3 , 100000)
 #auto2.marke = "VW" # Attribute gibt es nur in diesem Objekt
+auto3 = Auto("Audi", "grün", 6 , 10)
 
-print(Auto.anzahl_reifen)
-print(auto1,auto1.anzahl_reifen)
-print(auto2,auto2.anzahl_reifen)
+print("Anzahl Reifen",Auto.ANZAHL_REIFEN)
+print("Winterreifen notwendig",Auto.winterreifen_noetig)
+Auto.winterreifen_noetig = True
+print("Winterreifen notwendig",Auto.winterreifen_noetig)
+auto1.winterreifen_noetig = False
+auto1.ANZAHL_REIFEN = 6
+print(auto1,auto1.ANZAHL_REIFEN,auto1.winterreifen_noetig)
+print(auto2,auto2.ANZAHL_REIFEN,auto2.winterreifen_noetig)
 auto2.kilometerstand_ausgeben()
 auto2.fahren(30)
+print("Es gibt",Auto.anzahl_autos,"Autos")
