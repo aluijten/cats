ca0a516f335a18e2ebd2f10d4917199320a5e6e0
aluijten <a.luijten@gmx.at>
2025-11-15 13:15:00 +0100

    update auto Beispiele

--- auto.py
+++ auto.py
@@ -1,4 +1,14 @@
 class Auto:
-    pass
 
-auto1 = Auto()
+    def __init__(self, text):
+        print("Init methode -",text)
+
+
+auto1 = Auto("Hurra")
+#auto1.marke = "BMW"
+
+auto2 = Auto("Pfui")
+#auto2.marke = "VW"
+
+print(auto1)
+print(auto2)
